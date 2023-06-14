1c0d4fee8e1bd1b5211998f47c3f50c63045393b
liulei11 <liulei11@kuaishou.com>
2023-06-14 21:52:03 +0800

    feat: 修复聚合模型参数丢失

--- src/script/index.ts
+++ src/script/index.ts
@@ -1,89 +1,89 @@
 // @ts-nocheck
 
 function getDecodeString(fn: string) {
-  return fn
-    ? decodeURIComponent(fn).replace(
-        /export\s+default.*function.*\(/,
-        'function _RT_('
-      )
-    : fn;
+	return fn
+		? decodeURIComponent(fn).replace(
+			/export\s+default.*function.*\(/,
+			'function _RT_('
+		)
+		: fn;
 }
 
 function getScript(serviceItem, isTest = false) {
-  function fetch(params, { then, onError }, config) {
-    function serviceAgent(params, config) {
-      const method = `__method__`;
-      const path = `__path__`;
-
-      try {
-        const url = path;
-	      __convert_page_info__
-        const newParams = method.startsWith('GET') ? { params, url, method } : { data: params, url, method };
-        const options = __input__(newParams);
-        options.url = (options.url || url).replace(/{(\w+)}/g, (match, key) => {
-          const param = params[key] || '';
-          Reflect.deleteProperty(options.params || {}, key);
-          return param;
-        });
-        options.method = options.method || method;
-	      config
-          .ajax(options)
-		      .then((response) => {
-			      return response.data;
-		      })
-          .then((response) => {
-            return __output__(response, Object.assign({}, options), {
-	            throwStatusCodeError: (data) => {
-		            onError(data);
-	            },
-            });
-          })
-		      .then((response) => {
-			      return __convert_response__(response);
-		      })
-		      .then(then)
-          .catch((error) => {
-            onError((error && error.message) || error);
-          });
-      } catch (error) {
-        return onError(error);
-      }
-    }
-    return serviceAgent(params, config);
-  }
+	function fetch(params, { then, onError }, config) {
+		function serviceAgent(params, config) {
+			const method = `__method__`;
+			const path = `__path__`;
+			
+			try {
+				const url = path;
+				__convert_page_info__(params);
+				const newParams = method.startsWith('GET') ? { params, url, method } : { data: params, url, method };
+				const options = __input__(newParams);
+				options.url = (options.url || url).replace(/{(\w+)}/g, (match, key) => {
+					const param = params[key] || '';
+					Reflect.deleteProperty(options.params || {}, key);
+					return param;
+				});
+				options.method = options.method || method;
+				config
+				.ajax(options)
+				.then((response) => {
+					return response.data;
+				})
+				.then((response) => {
+					return __output__(response, Object.assign({}, options), {
+						throwStatusCodeError: (data) => {
+							onError(data);
+						},
+					});
+				})
+				.then((response) => {
+					return __convert_response__(response);
+				})
+				.then(then)
+				.catch((error) => {
+					onError((error && error.message) || error);
+				});
+			} catch (error) {
+				return onError(error);
+			}
+		}
+		return serviceAgent(params, config);
+	}
 	
 	let fetchString = fetch
-		.toString()
-		.replace('__input__', getDecodeString(serviceItem.input))
-		.replace('__output__', getDecodeString(serviceItem.output))
-		.replace('__method__', serviceItem.method)
-		.replace('__path__', serviceItem.path?.trim())
+	.toString()
+	.replace('__input__', getDecodeString(serviceItem.input))
+	.replace('__output__', getDecodeString(serviceItem.output))
+	.replace('__method__', serviceItem.method)
+	.replace('__path__', serviceItem.path?.trim())
 	
 	return encodeURIComponent(
-    isTest || serviceItem.modelType === 'domain'
-	    ? fetchString.replace('__convert_page_info__', '')
-	      .replace('__convert_response__', '(response => response)')
-	    : fetchString
-	      .replace('__convert_page_info__', serviceItem.pageInfo ? `
+		isTest || serviceItem.modelType === 'domain'
+			? fetchString.replace('__convert_page_info__', '(() => {})')
+			.replace('__convert_response__', '(response => response)')
+			: fetchString
+			.replace('__convert_page_info__', serviceItem.pageInfo ? `((params) => {
 	        if (!params) { return; }
 	        const pageNum = params.page ? params.page.pageNum : undefined;
 	        const pageSize = params.page ? params.page.pageSize : undefined;
 	        delete params.page;
 	        delete params.fields;
-	        params = { ...params, ...(params.query || {}) };
+	        Object.assign(params, params.query || {});
 	        delete params.query;
 	        delete params.orders;
 	        
 	        ${serviceItem.pageInfo.pageNumKey ? `params.${serviceItem.pageInfo.pageNumKey} = pageNum;` : ''}
 	        ${serviceItem.pageInfo.pageSizeKey ? `params.${serviceItem.pageInfo.pageSizeKey} = pageSize;` : ''}
-	      ` : `
+	      })` : `((params) => {
 	        if (!params) { return; }
 	        delete params.page;
 	        delete params.fields;
-	        params = { ...params, ...(params.query || {}) };
+	        Object.assign(params, params.query || {});
 	        delete params.query;
-	      `)
-	      .replace('__convert_response__', serviceItem.markedKeymap ? `((response) => {
+	      })`)
+			.replace('__convert_response__', serviceItem.markedKeymap ? `((response) => {
         const markedKeyMap = ${JSON.stringify(serviceItem.markedKeymap)};
         const newResponse = { code: 1, data: {} };
         
